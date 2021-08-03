08d9da4f102ff12e445c571b93b5fdecfc7a6f90
Felicio Santos <feliciosantoslive@outlook.com>
2021-08-03 09:42:46 -0300

    feat(price): changes price

--- src/common/utils/company.js
+++ src/common/utils/company.js
@@ -3,8 +3,8 @@ import differenceInDays from 'date-fns/differenceInDays';
 export const companyPlanLabels = (plan) => {
   const planLabels = {
     TRIAL: 'Avaliação gratuita',
-    DEFAULT: 'Padrão (R$ 14,90)',
-    CUSTOM: 'Personalizado (R$ 39,90)',
+    DEFAULT: 'Padrão (R$ 8,90)',
+    CUSTOM: 'Personalizado (R$ 25,90)',
   };
 
   return planLabels[plan];
